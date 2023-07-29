6839d664f871313b2d217ac06e87fdcd19259d5c
Raul Ribeiro Bonifácio <raul.ribeiro.bonifacio@hotmail.com>
2023-07-29 10:42:31 -0300

    - Theme configuration updated.

--- lua/plugins/configure_base16_vim.lua
+++ lua/plugins/configure_base16_vim.lua
@@ -1,7 +1,22 @@
 
 local function configure_base16_vim()
+	vim.api.nvim_create_autocmd({ "Colorscheme" }, {
+		group = vim.api.nvim_create_augroup('base16-vim', { clear = true }),
+		callback = function()
+			vim.cmd.highlight { "link Statusline Normal", bang = true }
+			vim.cmd.highlight { "link StatusLineNC Signcolumn", bang = true }
+			vim.cmd.highlight { "link WinSeparator Signcolumn", bang = true }
+			vim.cmd.highlight { "link TabLineSel Normal", bang = true }
+			vim.cmd.highlight { "link TabLineFill Signcolumn", bang = true }
+			vim.cmd.highlight { "link TabLine Signcolumn", bang = true }
+			vim.cmd.highlight { "link TabLine Signcolumn", bang = true }
+			vim.cmd.highlight { "link PmenuSel Normal", bang = true }
+			vim.cmd.highlight { "link Pmenu Signcolumn", bang = true }
+		end
+	})
+
 	if vim.env.BASE16_THEME then
-		vim.opt.termguicolors = false
+		vim.opt.termguicolors = true
 		vim.g.base16_colorspace = 256
 		vim.g.base16_background_transparent = 1
 		vim.cmd.colorscheme('base16-' .. vim.env.BASE16_THEME)

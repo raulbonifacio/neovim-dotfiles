bcdf508a851216ef2091c14719419e45151cc489
Raul Ribeiro Bonifácio <raul.ribeiro.bonifacio@hotmail.com>
2023-08-19 23:15:36 -0300

    - Configuration updated.

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require('plugins')
 require('options')
 require('mappings')
+require('autocmds')
 
 -- vim: sw=4 ts=4
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,2 +1,4 @@
 vim.keymap.set('n', '<leader>ce', '<cmd> edit $MYVIMRC <cr>')
 vim.keymap.set('n', '<leader>cr', '<cmd> luafile %<cr>')
+
+-- vim: sw=4 ts=4

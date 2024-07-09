10c25f5e9a4e6cd38c949878bb398322a8ea28e0
Raul Ribeiro Bonifácio <raul.ribeiro.bonifacio@hotmail.com>
2024-07-09 20:26:26 -0300

    - Configuration updated.

--- lua/options.lua
+++ lua/options.lua
@@ -15,14 +15,15 @@ vim.opt.number = true
 vim.opt.path:append { '**' }
 vim.opt.previewheight = 4
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 999
 vim.opt.signcolumn = 'yes:3'
 vim.opt.splitbelow = true
 vim.opt.statusline = '%F%q%m%=%5.5l,%-5.5c%y'
 vim.opt.tags:append({ '.ctags' })
 vim.opt.undodir = vim.env.HOME .. '/.config/nvim/undodir'
 vim.opt.undofile = true
+vim.opt.updatetime = 200
 vim.opt.wildoptions:append { 'fuzzy' }
 vim.opt.wrap = false
-vim.opt.updatetime = 200
 
 -- vim: sw=4 ts=4
